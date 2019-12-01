1f23d99112861998be2e58e8c8f91b4b12ea0e1d
c0dehard <47797039+c0dehard@users.noreply.github.com>
2019-12-01 22:02:56 +0100

    new function/method
    pass object to get a specific value of the key inside the method/function

--- Panda.py
+++ Panda.py
@@ -4,13 +4,26 @@ class Panda():
     def __init__(self,name,age,hungry):
         self.data = {"name":name,"age":age,"hungry":hungry}
         if self.data.get("hungry") == True:
-            print("I Am Hungry!")
+            print("I'am Hungry!")
 
 pan = Panda("Big Panda","N/A",True)
 
 print(f'And my name is: ,,{pan.data.get("name")}"')
 
-#test namenändern / change name test      
+
 pan.data["name"] = "c0deN@c0dehard"
 print(f'Name changed to: {pan.data.get("name")}')
-#YES i LOVE Format Strings!
+
+
+
+#add key  and value to a dict
+pan.data["favfood"] = "Bamboo"
+
+
+#insert any panda to get his favfood printed
+def favFoodOf(name):
+    hisfavfood = name.data.get("favfood")
+    print(f'{name.data.get("name")} likes {hisfavfood}')
+
+# just pass the object as parameter of this function
+favFoodOf(pan)
