85d46cdc36210bf037e1345e676585c5b06e2d03
c0deN <47797039+c0dehard@users.noreply.github.com>
2020-03-03 23:19:35 +0100

    not ment for CLI only
    made module not useable directly by calling it in a terminal

--- linearSearch.py
+++ linearSearch.py
@@ -1,14 +1,35 @@
+
+def hinweis():
+    print("""
+    Als Skript nicht ausführbar
+    muss importiert werden um Methoden nutzen zu können
+
+    import linearSearch
+    oder z.B. from linearSearch import lineareSuche
+
+    Beispiel:
+    from linearSearch import lineareSuche
+    ordner1 = ["audio.ogg","bild.png","beispiel.py","index.php"]
+    x = lineareSuche(ordner1,"beispiel.py")
+    print(x)
+    """)
+
 def lineareSuche(woSuchen,wasGesucht):
+    woSuchen,wasGesucht
     i, gefunden = 0, False
     while not gefunden and i < len(woSuchen):
         if wasGesucht == woSuchen[i]:
             gefunden = True
         i += 1
     if gefunden:
-        return f"{wasGesucht} ist dabei!"
+        return f"{wasGesucht} ist dabei"
     else:
-        return f"{wasGesucht} ist Nicht dabei!"
+        return f"{wasGesucht} ist Nicht dabei"
 
-# Test
-personen = ["alf","bert","claus","horst"]
-print(lineareSuche(personen,"bert"))
+# Beispiele
+# ordner1 = ["audio.ogg","bild.png","beispiel.py","index.php"]
+# print(lineareSuche(ordner1,"beispiel.py"))
+
+# Damit man das Modul nicht ausfersehen direkt einfach in der Konsole ausführt 
+if __name__ == "__main__":
+    hinweis()
