92521b5a043daddd8e213dd5a3640e2509de762b
c0deN <47797039+c0dehard@users.noreply.github.com>
2020-03-10 18:15:19 +0100

    Verständlicher gemacht

--- MVC.py
+++ MVC.py
@@ -1,25 +1,40 @@
 class Model:
     def modeldaten(self):
-        print('Model fragt Daten ab')
+        """
+        Mittels dieser Methode lassen sich die Daten erzeugen und zurückgeben
+        Dient jedoch nur zur Veranschaulichung
+        """
+        print('Model fragt Daten ab.. ')
         data = 'Model Daten könnten hier stehen'
         return data
 
 class View:
     def render(self, data):
-        print('Das Model wird im View gezeigt:' , data)
+        """
+        Zeigt in der Konsole die eingegebenen Daten an
+        """
+        print('Daten von Model werden im View gezeigt.. ' , data)
 
 class Controller:
-    # Initialisierung / Konstruktor
-    # Instanziierung von Model und View
+
     def __init__(self):
+        """
+        Initialisierung / Konstruktor
+        Instanziierung von Model und View
+        """
         self.model = Model()
         self.view = View()
-
 
     # Controller hat eine Methode welche auf Methoden
     # der instanziierten Klassen zugreifen kann
     def present(self):
-        print('Controller verwaltet Model und View')
+        """
+        Diese Methode hat natürlich Zugriff auf Methoden
+        der instanziierten Klassen
+        Daten werden generiert und letzendlich dem Benutzer präsentiert
+        """
+        print('Controller verwaltet also Model und View.. ')
+        # Beispieldaten erzeugt mit der demo Methode, dient nur zur Veranschaulichung
         data = self.model.modeldaten()
         self.view.render(data)
 
