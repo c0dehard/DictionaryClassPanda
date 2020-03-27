63ac1e84a2569b6ca7514efce0a2e832dbf46a3e
c0deN <47797039+c0dehard@users.noreply.github.com>
2020-03-27 17:01:01 +0100

    some changes, improved functions

--- trainingWh33ls.py
+++ trainingWh33ls.py
@@ -9,12 +9,12 @@ def check(elem, seq):
 	"""
     return elem in seq
 
-def reversedList(n,downToZero=True):
+def reversed_list(n,downToZero=True):
     """
     #### returns a list from n to lowest in a descending order
     ###  input has to be an integer
     #### Example:
-        x = reversedList(10,False)
+        x = reversed_list(10,False)
     ###  print(x)
     ### [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 	"""
@@ -26,11 +26,11 @@ def reversedList(n,downToZero=True):
         n-=1
     return list
 
-def countVowels(string):
+def count_vowels(string):
     """
     #### returns amount of vowels in given string
     #### Example:
-        x = countVowels("The meaning of life, 42 to proof this, is this text"*3)
+        x = count_vowels("The meaning of life, 42 to proof this, is this text"*3)
     ###  print(x)
     ### 42
 	"""
@@ -39,12 +39,12 @@ def countVowels(string):
         if i.lower() in vowels : counter+=1
     return counter
 
-def incrementArr(array,increment=1):
+def increment_arr(array,increment=1):
     """
-    #### returns the given array increments each number
+    #### returns an incremented array
     #### Example:
        x = [1,2,3,4,5]
-     incrementArr(x)
+     increment_arr(x)
     ###  print(x)
     ### [2, 3, 4, 5, 6]
 	"""
@@ -52,12 +52,12 @@ def incrementArr(array,increment=1):
         array[num]=array[num]+increment
     return array
 
-def decrementArr(array,decrement=1):
+def decrement_arr(array,decrement=1):
     """
-    #### returns the given array decrements each number
+    #### returns an decremented array
     #### Example:
        x = [1,2,3,4,5]
-     decrementArr(x)
+     decrement_arr(x)
     ###  print(x)
     ### [0, 1, 2, 3, 4]
 	"""
@@ -65,51 +65,57 @@ def decrementArr(array,decrement=1):
         array[num]=array[num]-decrement
     return array
 
-def ytvID(url):
+def ytv_id(url):
 	"""
-    #### returns YouTube Video ID from given URL
+    #### returns YouTube Video ID
     ##### wheter regular or shortened format
     ##### Example:
-	 x = ytvID(input('enter url: '))
+	 x = ytv_id(input('enter url: '))
     ###  print(x)
 	 >>> enter url: https://www.youtube.com/watch?v=jNQXAC9IVRw
     ### jNQXAC9IVRw
 	"""
-	vid = url.split('/');return vid[-1].split('watch?v=')[-1]
+	vid = url.split('/') ; return vid[-1].split('watch?v=')[-1]
 
-def justInts(string):
-    """
-    #### returns a list of integers 
-    ##### from a specified string
-    #### Example:
-        x = justInts("1.1 example now 2 or four words")
-    ###  print(x)
-     >>> ['1', '1', '2']
-    """
-    import re
-    return re.findall(r"\d+",string)
+def just_ints(string,as_int=True):
+	"""
+	#### returns a list of integers as string or integer
+	##### by default as integer list
+	#### Example:
+		x = just_ints("1.1 example now 2 or four words",False)
+	###  print(x)
+	 >>> ['1', '1', '2']
+	"""
+	import re
+	results = re.findall(r"\d+",string)
+	if as_int:
+		# TODO: need to learn more about -> map
+		# new solution is inspired by
+		# https://stackoverflow.com/questions/7368789/convert-all-strings-in-a-list-to-int
+		return list(map(int, results))
+	return results
 
-def allRand(upTo=50):
+def all_rand(upTo=50):
     """
     ### returns as many random numbers
     ### up to the given number in a list
     if nothing's set basic will be 50
     #### Example:
-        x = allRand(10)
+        x = all_rand(10)
     ###  print(x)
      >>> [9, 8, 2, 5, 2, 6, 2, 0, 4, 1]
     """
     x=[]
     import random
-    for nums in range(upTo):x.append(int(random.randrange(0,upTo)))
+    for nums in range(upTo) : x.append(int(random.randrange(0,upTo)))
     return x
 
-def avgWordLen(values,delimiter = " "):
+def avg_wordlen(values,delimiter = " "):
     """
     #### returns the average length of given values: 
     if not specified, space will be the basic delimiter
     #### Example:
-        x = avgWordLen("One two three four five six")
+        x = avg_wordlen("One two three four five six")
     ###  print("avg is: ",x)
      >>> avg is 3.6666666666666665
     """
@@ -122,5 +128,8 @@ def avgWordLen(values,delimiter = " "):
 # TODO Add more helpful functions
 """
 I didn’t knew python comments where formatable in vscode!
-Figured out accidentally, LOVE IT :D
+Figured out accidentally, LOVE IT..
+Well, maybe I'll just shrink the docstrings down as i regular write 'em
+it’s better for CLI e.g. if a user writes help(function) ..
+In my opinion are documentation strings very ugly to look they ain't formated.
 """
