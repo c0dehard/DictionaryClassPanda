4763b4740d3dba4d1f72636592f433912f938923
c0deN <47797039+c0dehard@users.noreply.github.com>
2020-09-06 01:07:29 +0200

    Add option to set size

--- faviconPNGdownload.py
+++ faviconPNGdownload.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 import urllib.request
 url = input("Enter URL> ")
+img_size=input("Size (e.g.) 32, 64, 128> ")
 filename = url.split('/')[-1]
-urllib.request.urlretrieve(f"http://www.google.com/s2/favicons?sz=64&domain_url={url}", filename+".png")
+urllib.request.urlretrieve(f"http://www.google.com/s2/favicons?sz={img_size}&domain_url={url}", filename+".png")
