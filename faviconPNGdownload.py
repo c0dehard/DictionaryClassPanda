31a7ec48e2f8d1af61fafe45b1ba97e45afa8025
c0deN <47797039+c0dehard@users.noreply.github.com>
2020-09-06 01:15:55 +0200

    Refactor variables, Add size to filename
    Change:
    - img_size -> size
    - filename -> file
    Add:
    - size to -> file string to extend filename

--- faviconPNGdownload.py
+++ faviconPNGdownload.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 import urllib.request
 url = input("Enter URL> ")
-img_size=input("Size (e.g.) 32, 64, 128> ")
-filename = url.split('/')[-1]
-urllib.request.urlretrieve(f"http://www.google.com/s2/favicons?sz={img_size}&domain_url={url}", filename+".png")
+size=input("Size (e.g.) 32, 64, 128> ")
+file= f"{url.split('/')[-1]}{size}x{size}.png"
+urllib.request.urlretrieve(f"http://www.google.com/s2/favicons?sz={size}&domain_url={url}", file)
