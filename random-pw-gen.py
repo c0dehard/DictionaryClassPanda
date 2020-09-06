e0ea0e17e5bc7ecc212fbfaf11c1fa44c7332724
c0deN <47797039+c0dehard@users.noreply.github.com>
2020-09-06 23:28:43 +0200

    Refactor
    [Update] set_pw() ,update_textfield() and gen_pw() shrunken down to gen_pw only .
    [Add] some colors (just for fun).
    [Fix] btn was causing issues. Changed the way how GUI works: 1. Set size -> 2. Generate by click how often you want.

--- random-pw-gen.py
+++ random-pw-gen.py
@@ -3,31 +3,23 @@ import string
 try:
     from tkinter import *
 except ImportError : from Tkinter import *
+h,w = 200,100
 
 window = Tk() ; window.title('Password Generator')
-window.geometry('200x100') ; window.configure(background='#ffffff')
-
-def gen_pw(scale):
-    pw = ''.join(random.choice(string.ascii_letters+string.punctuation+string.digits) for i in range(random.randint(scale,scale)))
-    set_pw(pw)
+window.geometry(f'{h}x{w}') ; window.configure(background='#282a36')
     
-def set_pw(text):
+def gen_pw():
     text_field.delete(0,"end")
-    text_field.insert(0, text)
-    
-def update_textfield(do_you_see_the_bug):
-    gen_pw(pw_scale.get())
+    text_field.insert(0, ''.
+                      join(random.choice(string.ascii_letters+string.punctuation+string.digits)
+                           for i in range(random.randint(pw_scale.get(),pw_scale.get()))))
 
-
-text_field = Entry(window,width = 100)
-pw_scale = Scale(window, from_=0, to=100,orient=HORIZONTAL,command=update_textfield)
-btn = Button(text="Refresh",command=update_textfield,width = 100)
-
+text_field = Entry(window,width = w,bg='#000000',foreground='#50fa7b')
+pw_scale = Scale(window, from_=0, to=100,orient=HORIZONTAL,bg='#282a36',foreground='white')
+btn = Button(text="Generate",command=gen_pw,width = w,bg='#000000')
 
 for item in [text_field, pw_scale,btn]:
 	item.pack()
 
-
 if __name__ == '__main__':
     mainloop()
-
